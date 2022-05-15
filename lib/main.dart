807b1cc2e14e5af64d81a67b7efb14a2fd9e5959
mikhael_mike <mnosson@gmail.com>
2022-05-16 01:52:23 +0400

    filters

--- lib/main.dart
+++ lib/main.dart
@@ -6,32 +6,53 @@ import 'package:navigation_meals_app/screens/meals_screen_categories.dart';
 import 'package:navigation_meals_app/screens/tabs_screen.dart';
 
 void main() => runApp(MaterialApp(
-  theme: ThemeData(accentColor: Colors.blue),
+      theme: ThemeData(accentColor: Colors.blue),
+      initialRoute: '/',
+      routes: {
+        // '/':(context) => TabsScreen(),
+        MealsScreenCategories.routName: (context) => MealsScreenCategories(),
+        MealDetailScreen.routeName: ((context) => MealDetailScreen()),
+        FiltersScreen.routName: ((context) => FiltersScreen(_setFilter)),
+      },
+      home: MyApp(),
+      onGenerateRoute: (settings) {
+        print(settings.arguments);
+        return MaterialPageRoute(builder: (context) => CategoriesScreen());
+      },
+    ));
+
+    
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
   
-  initialRoute: '/',
-  routes: {
-    // '/':(context) => TabsScreen(),
-    MealsScreenCategories.routName :(context) => MealsScreenCategories(),
-    MealDetailScreen.routeName : ((context) => MealDetailScreen()),
-    FiltersScreen.routName: ((context) => FiltersScreen()),
-  },
-  home: MyApp(),
-  onGenerateRoute: (settings) {
-    print(settings.arguments);
-    return MaterialPageRoute(builder: (context) => CategoriesScreen());
-  },
-));
-
-
-class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    Map<String, bool> _filters = {
+    'Gluten': false,
+    'Lacoste': false,
+    'Vegan': false,
+    'Vegetarian': false,
+  };
+
+  void _setFilters(Map<String, bool> filterData) {
+    setState(() {
+      _filters = filterData;
+    });
+  }
+    return MaterialApp(
       
-      body: TabsScreen(),
-      
+      home: Scaffold(
+        body: TabsScreen(),
+      ),
     );
   }
 }
--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,17 +1,82 @@
 import 'package:flutter/material.dart';
 import 'package:navigation_meals_app/widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static final routName = '/filter-screen';
-  const FiltersScreen({Key? key}) : super(key: key);
+  // const FiltersScreen({Key? key}) : super(key: key);
+
+  final Function() saveFilter;
+
+  FiltersScreen(this.saveFilter);
+
+
+  @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  bool _glutenFree = false;
+  bool __vegetarian = false;
+  bool _vegan = false;
+  bool _LactoseFree = false;
+
+  Widget _buildSwitchTitle(String title, String description, bool currenctValue,
+      Function(bool) updateValue) {
+    return SwitchListTile(
+        title: Text(title),
+        subtitle: Text(description),
+        value: currenctValue,
+        onChanged: updateValue
+        );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Your filters')),
+      appBar: AppBar(
+        title: Text('Your filters'),
+        actions: <Widget>[
+          IconButton(onPressed: widget.saveFilter, icon: Icon(Icons.save)),
+        ],
+        ),
       drawer: MainDrawer(),
-      body: Center(child: Text('Filters screen')),
-    );
     
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Adjust your meal selection',
+              style: TextStyle(fontSize: 30),
+            ),
+          ),
+          Expanded(
+              child: ListView(
+            children: <Widget>[
+              _buildSwitchTitle('Gluten-Free', 'Only include Gluten-free meals', _glutenFree, (newValue) {
+                setState(() {
+                  _glutenFree = newValue;
+                });
+              }),
+              _buildSwitchTitle('Lactose-Free', 'Only include Lactose-free meals', _LactoseFree, (newValue) {
+                setState(() {
+                  _LactoseFree = newValue;
+                });
+              }),
+              _buildSwitchTitle('Vegetarian', 'Only include Vegetarian meals', __vegetarian, (newValue) {
+                setState(() {
+                  __vegetarian = newValue;
+                });
+              }),
+              _buildSwitchTitle('Vegan', 'Only include Vegen meals', _vegan, (newValue) {
+                setState(() {
+                  _vegan = newValue;
+                });
+              }),
+            ],
+          ))
+        ],
+      ),
+    );
   }
 }
